6d76bc0915e0720effb231c6ccc7ee5db170802d
GevorgSafaryan <gevorgsafarjan@gmail.com>
2017-04-08 13:30:47 +0400

    For assert

--- src/test/java/aca/first/test/MyFirstWebDriverTest.java
+++ src/test/java/aca/first/test/MyFirstWebDriverTest.java
@@ -3,6 +3,7 @@ package aca.first.test;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -13,13 +14,12 @@ Precondition: The user have ID both in sport and in casino
 Steps:
 1. Open any browser,
 2. Go to https://totogaming.am,
-3. Go to Sport,
-4. Press the Login button,
-5. In the opened window enter valid login and password,
-6. Press login,
-7. Press on login wich is written in the right top of the page,
-8. From the opened choose transfer,
-9. In the opened window enter the Casino ID, the amount and press confirm
+3. Press the Login button,
+4. In the opened window enter valid login and password,
+5. Press login,
+6. Press on login wich is written in the right top of the page,
+7. From the opened choose transfer,
+8. In the opened window enter the Casino ID, the amount and press confirm
 Expected result: The Sport ID will decrease and the Casino ID will increase by the same amount
 */
 	
@@ -30,8 +30,14 @@ Expected result: The Sport ID will decrease and the Casino ID will increase by t
 		 WebDriver driver = new ChromeDriver();
 		 driver.get("http://sport.totogaming.am");
 		 driver.findElement(By.xpath("//*[@id=\"main_data_panel\"]/div[2]/div[1]/div[1]/div[1]"));
-		 driver.close();
-		 driver.quit();
+		 driver.findElement(By.xpath("//*[@id=\"login_panel\"]/a[2]")).click();
+		 WebElement login = driver.findElement(By.xpath("//*[@id=\"login\"]"));
+		 login.sendKeys("totobasket");
+		 WebElement password = driver.findElement(By.xpath("//*[@id=\"password\"]"));
+		 password.sendKeys("toto1");
+		 driver.findElement(By.xpath("//*[@id=\"login_dialogue\"]/input")).click();
+		 //driver.close();
+		 //driver.quit();
 	}
 
 }
